57ec42c5088cab825227608b1ec24dece79fd99d
Jackson <jacksongraff@hotmail.com>
2024-01-27 21:04:11 -0300

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactamplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createTodo, deleteTodo} from './graphql/mutations'
+import { listTodos } from './graphql/queries'
+import { withAuthenticator, Button, Text, Flex, Heading } from "@aws-amplify/ui-react";
+import { useCallback, useEffect, useState } from 'react';
+import { Amplify } from "aws-amplify";
+import { generateClient } from "aws-amplify/api";
+import awsconfig from "./amplifyconfiguration.json";
 
-function App() {
+Amplify.configure(awsconfig);
+
+const API = generateClient();
+
+function App({ signOut }: {signOut?: () => void}) {
+  const [ todos, setTodos ] = useState<{id: string,name: string}[]>([])
+
+  const fetchtodos = useCallback(async () => {
+    const result = await API.graphql({
+      query: listTodos,
+    })
+    const todosFromAPI = result.data.listTodos.items;
+    setTodos(todosFromAPI);
+  }, [setTodos])
+
+  const handleCreateTodo = useCallback(async () => {
+    await API.graphql({
+      query: createTodo,
+      variables: { input: { name: window.prompt("New note") ?? '' } }
+    })
+    fetchtodos()
+  }, [fetchtodos])
+
+  const handleDeleteTodo = useCallback(async (id: string) => {
+    await API.graphql({
+      query: deleteTodo,
+      variables: { input: { id: id } }
+    })
+    fetchtodos()
+  }, [fetchtodos])
+
+  useEffect(() => {
+    fetchtodos()
+  }, [fetchtodos])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex direction={"column"}>
+      <Flex justifyContent={'space-between'}>
+        <Heading level={1}>My todos</Heading>
+        <Button onClick={signOut}>Sign Out</Button>
+      </Flex>
+      {todos.map(note => <Flex alignItems={'center'}>
+        <Text>{note.name}</Text>
+        <Button onClick={() => handleDeleteTodo(note.id)}>Remove</Button>
+      </Flex>)}
+      <Button onClick={handleCreateTodo}>Add Note</Button>
+    </Flex>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
